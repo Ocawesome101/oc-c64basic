2879567da53aba89ba8191e6af26f24d2467ad7c
Ocawesome101 <ocawesome101@gmail.com>
2020-09-30 02:07:07 -0400

    hoooooooooo boy. What have I gotten myself into?

---
+++ init.lua
@@ -0,0 +1,28 @@
+-- basics --
+
+_G.sys = {}
+
+sys.gpu = component.proxy(component.list("gpu", true)())
+sys.mounts = {[0] = component.proxy(computer.getBootAddress())}
+sys.gpu.bind(component.list("screen", true)())
+
+function sys.loadfile(drv, file)
+  checkArg(1, file, "string")
+  local handle = assert(sys.mounts[drv].open(file))
+  local data = ""
+  repeat
+    local chunk = sys.mounts[drv].read(handle, math.huge)
+    data = data .. (chunk or "")
+  until not chunk
+  sys.mounts[drv].close(handle)
+  if file:sub(-4) == ".lua" then
+    return load(data, "="..file, "bt", _G)
+  else
+    return function() return sys.run(data) end
+  end
+end
+
+function sys.dofile(d,f) return sys.loadfile(d,f)() end
+
+sys.dofile(0, "screen.lua")
+sys.dofile(0, "basic.lua")
---
+++ screen.lua
@@ -0,0 +1,6 @@
+-- screen I/O --
+
+sys.dofile(0, "font.lua")
+
+local text = {
+}
